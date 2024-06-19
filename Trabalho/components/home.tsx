f0bcf0b272b68177b1fb1343ba90694a7b30460b
Marlonsil <marlonsilveira397@gmail.com>
2024-06-19 12:34:26 +0000

    finaumente

--- Trabalho/components/home.tsx
+++ Trabalho/components/home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity,Image } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import Sobre from './sobre';
 
 export default function App() {
@@ -9,17 +9,20 @@ const renderPage = () => {
     if (page === 'home') {
         return (
             <View style={styles.container}>
-                <Text style={styles.title}> Localize Aqui</Text>
+                <Text style={styles.Logo}> Localize <Text style={styles.Logo2}>Aqui</Text> </Text>
+                <View style={styles.line}></View>
+
                 <Text style={styles.title}> Quer saber onde você está agora?</Text>
-                <Text style={styles.title}>Descubra seu ponto exato no Mapa</Text>
-              
-                <Image
-        source={require('../../components/celular.png')}
-        style={styles.logo}
-    />
+                <Text style={styles.testo}>Descubra seu ponto exato no Mapa</Text>
+                <Text style={styles.mapa}>Mapa</Text>
+
+                <View style={styles.imageContainer}>
+                <View style={styles.circle}></View>
+                <Image style={styles.Ima} source={require('../assets/images/celu.png')} />
+</View>
 
                 <TouchableOpacity style={styles.button} onPress={() => setPage('sobre')}>
-                    <Text style={styles.buttonText}>Clique aqui</Text>
+                    <Text style={styles.buttonText}>Entrar</Text>
                 </TouchableOpacity>
             </View>
     );
@@ -36,24 +39,83 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: '#000C29',
     },
     title: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: '#000',
+    color: '#fff',
     marginBottom: 20,
     },
+    testo: {
+        fontSize: 24,
+        
+        color: '#fff',
+        marginBottom: 20,
+        },
+        mapa: {
+            fontSize: 24,
+           
+            color: '#0F77B1',
+            marginBottom: 20,
+            },
+    Logo: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#fff',
+        marginBottom: 20,
+        marginRight:225,
+        },
+        Logo2: {
+            fontSize: 24,
+            fontWeight: 'bold',
+            color: '#0F77B1',
+            marginBottom: 20,
+            },
+            line: {
+                width: '40%', // Largura da linha (80% da largura do container)
+                height: 2, // Altura da linha
+                backgroundColor: '#0F77B1', // Cor da linha
+                marginBottom: 10, // Espaço abaixo da linha
+                marginRight:220,
+                marginTop:-15,
+              },
     button: {
-    backgroundColor: '#007BFF',
-    padding: 10,
+    backgroundColor: '#071D92',
+    padding: 20,
+    paddingHorizontal:100,
     borderRadius: 5,
     },
     buttonText: {
     color: '#FFFFFF',
     fontSize: 16,
+    fontWeight: 'bold',
     },
-
+    circle: {
+        marginTop:50,
+        width: 100,
+        height: 100,
+        borderRadius: 60,
+        backgroundColor: '#000C29', // Cor do círculo
+        position: 'absolute',
+        zIndex: -1, // Coloca o círculo atrás da imagem
+        borderWidth: 0, // Adiciona uma borda para melhorar o efeito
+        borderColor: 'rgba(15, 119, 177, 0.5)', // Cor da borda com transparência
+        shadowColor: '#fff', // Cor da sombra
+        shadowOffset: { width: 0, height: 100 }, // Offset da sombra
+        shadowOpacity: 1.0, // Opacidade da sombra
+        shadowRadius: 150, // Raio da sombra
+      },
+      imageContainer: {
+        alignItems: 'center',
+        marginBottom: 20,
+      },
+Ima:{
+    width: 400,
+    height: 400,
+    marginBottom: 20,
+    resizeMode: 'contain',
+},
     logo: {
         width: 100, 
         height: 100, 
--- Trabalho/components/sobre.tsx
+++ Trabalho/components/sobre.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import { Text, View, StyleSheet, TouchableOpacity,  Image } from 'react-native';
 import Home from './home';
 export default function App() {
 const [page, setPage] = React.useState('sobre');
@@ -8,9 +8,24 @@ const renderPage = () => {
 if (page === 'sobre') {
 return (
 <View style={styles.container}>
-        <Text style={styles.title}>Página Sobre</Text>
+
+                <Text style={styles.Logo}> Localize <Text style={styles.Logo2}>Aqui</Text> </Text>
+                <View style={styles.line}></View>
+
+                <Image style={styles.Ima} source={require('../assets/images/loca.png')} />
+
+        <Text style={styles.title}> Localização atual</Text>
+        <Text style={styles.title}>latitude 0:0</Text>
+        <Text style={styles.title}>longitude 0:0</Text>
+
+
+        <TouchableOpacity style={styles.button2} onPress={() => setPage("home")}>
+        <Text style={styles.buttonText2}>Obiter localização</Text>
+</TouchableOpacity>
+
+
         <TouchableOpacity style={styles.button} onPress={() => setPage("home")}>
-            <Text style={styles.buttonText}>Clique aqui</Text>
+            <Text style={styles.buttonText}>Voltar</Text>
         </TouchableOpacity>
     </View>
 );
@@ -27,21 +42,56 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff',
+    backgroundColor: '#000C29',
     },
     title: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: '#000',
+    color: '#fff',
     marginBottom: 20,
     },
-    button: {
-    backgroundColor: '#007BFF',
-    padding: 10,
-    borderRadius: 5,
-    },
-    buttonText: {
-    color: '#FFFFFF',
-    fontSize: 16,
-    },
+    Logo: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#fff',
+        marginBottom: 20,
+        marginRight:225,
+        },
+        Logo2: {
+            fontSize: 24,
+            fontWeight: 'bold',
+            color: '#0F77B1',
+            marginBottom: 20,
+            },
+            line: {
+                width: '40%', // Largura da linha (80% da largura do container)
+                height: 2, // Altura da linha
+                backgroundColor: '#0F77B1', // Cor da linha
+                marginBottom: 10, // Espaço abaixo da linha
+                marginRight:220,
+                marginTop:-15,
+              },
+              button: {
+                backgroundColor: '#071D92',
+                padding: 20,
+                paddingHorizontal:100,
+                borderRadius: 5,
+                },
+                buttonText: {
+                color: '#FFFFFF',
+                fontSize: 16,
+                fontWeight: 'bold',
+                },
+                button2: {
+                    margin:20,
+                    backgroundColor: '#fff',
+                    padding: 20,
+                    paddingHorizontal:100,
+                    borderRadius: 5,
+                    },
+                    buttonText2: {
+                    color: '#071D92',
+                    fontSize: 16,
+                    fontWeight: 'bold',
+                    },
     });
